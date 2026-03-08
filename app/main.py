634228b0d0969814b69f206ce3f5c15bf4cb2f79
Black-machine09 <domingosmbala325@gmail.com>
2026-03-08 15:01:37 +0000

    Allow localhost any port via CORS regex

--- app/main.py
+++ app/main.py
@@ -39,6 +39,7 @@ else:
 app.add_middleware(
     CORSMiddleware,
     allow_origins=allowed_origins,
+    allow_origin_regex=r"^https?://(localhost|127\.0\.0\.1)(:\d+)?$",
     allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
